9139feb58e0b82e331276c606cc31ad3f04aa3b9
Babar-Jawad-Anjum <89509895+Babar-Jawad-Anjum@users.noreply.github.com>
2024-09-19 00:54:12 +0500

    feat: socket url updated 2.0

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
-    // origin: "http://127.0.0.1:5173",
-    origin: "https://real-estate-api-d8bc.onrender.com",
+    origin: "http://127.0.0.1:5173",
   },
 });
 
